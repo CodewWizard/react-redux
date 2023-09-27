6270f0d822b64c1681eba210bc5a084ef8cd7199
Madiha Mujawar <madihamujawar410@gmail.com>
2023-09-27 13:01:51 +0530

    connecting redux toolkit state

--- 02-redux-basic-start/src/store/index.js
+++ 02-redux-basic-start/src/store/index.js
@@ -1,5 +1,5 @@
 import {createStore} from 'redux';
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, configureStore} from '@reduxjs/toolkit';
 
 const initialState = {
     counter : 0,
@@ -7,7 +7,7 @@ const initialState = {
 }
 
 // preparing a slice of our global space
-createSlice({
+const counterSlice = createSlice({
     name : 'counter',
     initialState,
     reducers : {
@@ -26,47 +26,9 @@ createSlice({
     }
 })
 
-
-const counteReducer =(state = initialState, action)=>{
-    if(action.type === 'increment'){
-        return{
-            counter : state.counter + 1,
-            showCounter: state.showCounter
-        }
-    }
-
-    if(action.type === 'decrement'){
-        return{
-            counter : state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-
-    if(action.type === 'increase'){
-        return{
-            counter : state.counter + action.amount,
-            showCounter : state.showCounter
-        }
-    }
-
-    if(action.type === 'reset'){
-        return{
-            counter : 0,
-            showCounter : state.showCounter
-        }
-    }
-
-    if(action.type === 'toggle'){
-        return{
-            counter : state.counter,
-            showCounter: !state.showCounter
-        }
-    }
-
-    return state;
-}
-
-const store = createStore(counteReducer);
+const store = configureStore({
+    reducer : counterSlice.reducer
+});
 
 export default store;
 
